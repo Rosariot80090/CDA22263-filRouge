7bb71ae06b4705c79438101531d91c72f8a36779
Rosariot80090 <grosariot@yahoo.fr>
2023-04-24 09:46:29 +0200

    miseAJour24042023

---
+++ Persistance_des_Donnees/Fil_rouge_requete.sql
@@ -0,0 +1,29 @@
+-- Active: 1679136074823@@127.0.0.1@3306@villagegreen
+
+--tableau de bord de gestion permettra d'afficher des indicateurs de performance
+
+--1. Chiffre d'affaires mois par mois pour une année sélectionnée
+
+
+
+--2. Chiffre d'affaires généré pour un fournisseur
+
+
+
+--3. TOP 10 des produits les plus commandés pour une année sélectionnée (référence et nom du produit, quantité commandée, fournisseur)
+
+
+
+--4. TOP 10 des produits les plus rémunérateurs pour une année sélectionnée (référence et nom du produit, marge, fournisseur)
+
+
+
+--5.Top 10 des clients en nombre de commandes ou chiffre d'affaires
+
+
+
+--6. Répartition du chiffre d'affaires par type de client
+
+
+
+--7. Nombre de commandes en cours de livraison.
